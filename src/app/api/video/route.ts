b6d716a993804f22c170f9a55469f868ce91bea0
Jinnapat Yana <genesoft.ai@gmail.com>
2024-02-15 23:37:05 +0700

    add upload page

---
+++ src/app/api/video/route.ts
@@ -0,0 +1,38 @@
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes, listAll } from "firebase/storage";
+import { NextRequest, NextResponse } from "next/server";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgW21sG6wqBeyqSWbEgvTp-YHbvtTDxRo",
+  authDomain: "hologram-a8137.firebaseapp.com",
+  projectId: "hologram-a8137",
+  storageBucket: "hologram-a8137.appspot.com",
+  messagingSenderId: "691906502947",
+  appId: "1:691906502947:web:0bb47e4d3781ec37cc6c96",
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export async function GET() {
+  const listRef = ref(storage, "/");
+  const getResult = await listAll(listRef);
+  getResult.items.forEach((itemRef) => {
+    console.log(itemRef.fullPath);
+  });
+  return new NextResponse();
+}
+
+export async function POST(request: NextRequest) {
+  const formData = await request.formData();
+  const refName = ("video/" + formData.get("name")) as string;
+  const video = formData.get("video") as File;
+  const videoRef = ref(storage, refName);
+  const uploadResult = await uploadBytes(videoRef, video);
+  if (!uploadResult) {
+    return new NextResponse("cannot upload file", {
+      status: 500,
+    });
+  }
+  return new NextResponse("ok");
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,121 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { ChangeEventHandler, useRef, useState } from "react";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+
+  const selectFile = () => {
+    if (!inputRef.current) return;
+    inputRef.current.click();
+  };
+
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const fileList = e.currentTarget.files;
+    if (!fileList) return;
+    setFile(fileList[0]);
+  };
+
+  const upload = async () => {
+    setErrorMessage("");
+    setIsProcessing(true);
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("name", file.name);
+    formData.append("video", file);
+    const uploadResult = await fetch("/api/video", {
+      method: "POST",
+      body: formData,
+    });
+    setIsProcessing(false);
+    console.log(uploadResult);
+    if (uploadResult.status != 200) {
+      setErrorMessage(await uploadResult.text());
+      return;
+    }
+    setFile(null);
+    setShowPopup(true);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-full flex flex-col items-center p-5">
+      <input
+        type="file"
+        hidden
+        accept="video/*"
+        ref={inputRef}
+        onChange={handleFileChange}
+      />
+      <div className="flex flex-col rounded-lg shadow-lg bg-gray-200 max-w-3xl w-full p-7 items-center gap-y-7">
+        <div className="flex flex-col items-center">
+          <h1 className="font-bold text-2xl">Upload New Video</h1>
+          <p>select a video to be uploaded to our storage</p>
         </div>
+        <div className="border h-0 border-gray-400 w-full"></div>
+        {!file && (
+          <button
+            className="rounded-lg bg-red-500 hover:bg-red-600 p-5 w-48 transition-colors duration-300 h-48 flex flex-col items-center justify-center"
+            onClick={selectFile}
+          >
+            <Image
+              src="/video-chat.png"
+              width={100}
+              height={100}
+              alt="upload image"
+            />
+            <p className="text-white">select a video</p>
+          </button>
+        )}
+        {file && <video src={URL.createObjectURL(file)}></video>}
+        <div className="flex flex-row justify-evenly items-center gap-5">
+          <button
+            onClick={selectFile}
+            disabled={isProcessing}
+            className="bg-orange-500 rounded-lg shadow-md hover:bg-orange-600 p-3 text-white transition-colors duration-300 disabled:bg-gray-500 disabled:text-gray-200"
+          >
+            {file ? "Change video" : "Select video"}
+          </button>
+          <button
+            className="bg-green-500 rounded-lg shadow-md hover:bg-green-600 p-3 text-white transition-colors duration-300 disabled:bg-gray-500 disabled:text-gray-200 flex flex-row gap-2 justify-center items-center"
+            onClick={upload}
+            disabled={file == null || isProcessing}
+          >
+            {isProcessing && (
+              <FontAwesomeIcon className="animate-spin" icon={faSpinner} />
+            )}
+            Upload video
+          </button>
+        </div>
+        <p className="text-red-700">{errorMessage}</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      {showPopup && (
+        <div className="bg-gray-100 rounded-2xl shadow-xl flex flex-col items-center p-8 absolute border-2 top-10 gap-6 w-80">
+          <p className="font-bold text-2xl">Upload Successful</p>
+          <Image
+            src="/high-five.png"
+            alt="successful image icon"
+            width={200}
+            height={200}
+          />
+          <p className="text-center">
+            Your video have been uploaded successfully. Close this window the
+            upload more video!
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <button
+            className="rounded-lg bg-blue-600 text-white p-3"
+            onClick={() => setShowPopup(false)}
+          >
+            Awesome!
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
